c978c90d1a755b49d87b2adcddb53a9bd41043ec
simminjeong <pooh5045@naver.com>
2023-08-17 11:00:33 +0900

    di.xml01 생성자로 주입

--- spring-workspace/springdi/src/main/java/di/xml01/Car.java
+++ spring-workspace/springdi/src/main/java/di/xml01/Car.java
@@ -1,8 +1,13 @@
 package di.xml01;
 
+import java.util.List;
+
 public class Car {
 	
 	private Tire tire;
+	private String carName;
+	private int year;
+	private List<String> driver;
 	
 	public Car() {
 		System.out.println("car() 의 생성자입니다.");
@@ -12,16 +17,31 @@ public class Car {
 		this.tire=t;
 		System.out.println("car(Tire t)의 생성자입니다.");
 	}
-
-	public Tire getTire() {
-		return tire;
-	}
-
-	public void setTire(Tire tire) {
-		System.out.println("setTire(Tire t)의 생성자입니다.");
-		this.tire = tire;
+	
+	public Car(String carname,Tire t) {
+		this.tire=t;
+		System.out.println("car(String carname,Tire t)의 생성자입니다.");
 	}
 	
+//	public Car(Tire t,String carname) {
+//		this.tire=t;
+//		System.out.println("car(Tire t,String carname)의 생성자입니다.");
+//	}
+	
+	public Car(String carname,int year,Tire t) {
+		this.tire=t;
+		System.out.println("car(String carname,int year,Tire t)의 생성자입니다.");
+	}
+	
+
+	public List<String> getDriver() {
+		return driver;
+	}
+
+	public void setDriver(List<String> driver) {
+		this.driver = driver;
+	}
+
 	public void printTireBrand() {
 		System.out.println("타이어 브랜드는  "+ tire.getTireBrand());
 	}
