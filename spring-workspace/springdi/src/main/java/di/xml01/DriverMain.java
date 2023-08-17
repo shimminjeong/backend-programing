3ae07f00b35a882436bfff8b9e5e78dc15434eae
simminjeong <pooh5045@naver.com>
2023-08-17 13:33:34 +0900

    di.anno02 @resource

--- spring-workspace/springdi/src/main/java/di/xml01/DriverMain.java
+++ spring-workspace/springdi/src/main/java/di/xml01/DriverMain.java
@@ -35,10 +35,7 @@ public class DriverMain {
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-        
-
-
-		
+        	
 
 	}
 
